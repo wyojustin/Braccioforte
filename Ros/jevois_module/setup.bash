84bc267ec59ebaae4d4b65954e20a3ef6e4ca8da
Justin Shaw <wyojustin@gmail.com>
2019-02-09 13:28:27 +0100

    added ArUco requirements

---
+++ Ros/jevois_module/setup.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# generated from catkin/cmake/templates/setup.bash.in
+
+### run prior to running any ros scripts
+CATKIN_SHELL=bash
+
+source /home/niryo/catkin_ws/devel/setup.sh
